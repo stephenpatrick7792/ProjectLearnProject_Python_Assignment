a69e13d375fe05e671515e079453df1ec9b0e053
stephenpatrick7792 <56256444+stephenpatrick7792@users.noreply.github.com>
2025-08-05 04:09:38 +0100

    Add files via upload

---
+++ week 1_assignment.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print ({f"The sum of {num1} and {num2} is: ", 
+num1+num2})
+print ({f"The sum of {num1} and {num2} is: ", 
+num1-num2})
+print ({f"The sum of {num1} and {num2} is: ", 
+num1*num2})
+print ({f"The sum of {num1} and {num2} is: ", 
+num1/num2})
+print ({f"The sum of {num1} and {num2} is: ", 
+num1%num2})
+
+print(num1+num2)
